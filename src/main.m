44d4aa86f36e794f46babcbaf516245a15a342e7
hanxu1997 <807174205@qq.com>
2017-12-13 15:58:49 +0800

    报告ing

--- src/main.m
+++ src/main.m
@@ -9,28 +9,33 @@ figure(1);
 subplot(1,2,1);
 imshow(gray_img);
 title('灰度图');
+imwrite(gray_img, '..\output\gary_img.jpg');
+
 % 1.通过阈值进行前后景分割（将线条和A4纸分开）
-thresh_result = thresholding(input_img);
+thresh_result = im2bw(gray_img, 0.75);
 subplot(1,2,2);
 imshow(thresh_result);
 title('阈值前后景分割');
+imwrite(thresh_result, '..\output\thresh_result.jpg');
 % 2.轮廓检测
-% 解决方法之一：形态学图像处理（膨胀腐蚀）
+% 形态学图像处理（膨胀腐蚀）
 imdilate_result = imdilate_imerode(thresh_result);
+% 为方便连通区域选择，二值图取反
 imdilate_result = ~imdilate_result;
 figure(2);
 subplot(1,2,1);
 imshow(imdilate_result);
 title('膨胀腐蚀');
+imwrite(imdilate_result, '..\output\imdilate_result.jpg');
 % 检测轮廓
 %获取连通区域，并进行显示 
 % imclearborder:边界对象抑制
 IM2 = imclearborder(imdilate_result,8);
 % bwareaopen:删除小面积对象
-BW2 = bwareaopen(IM2,59,8);
+BW2 = bwareaopen(IM2,60,8);
 figure(2);
-[B,L] = bwboundaries(BW2,'noholes');%寻找边缘，不包括孔  
-% imshow(label2rgb(L, @jet, [.5 .5 .5]))%显示图像 
+%寻找边缘，不包括孔
+[B,L] = bwboundaries(BW2,'noholes');  
 subplot(1,2,2);
 imshow(L); 
 title('轮廓检测');
@@ -44,22 +49,23 @@ end
 % 3.通过ROI将每张图片输出 
 %获取区域的'basic'属性， 'Area', 'Centroid', and 'BoundingBox'   
 stats = regionprops(BW2, 'basic');  
-centroids = cat(1, stats.Centroid);  
-%绘制开操作之后的二值化图像  
+%绘制感兴趣区域ROI
 figure('name','ROI区域提取');
 imshow(input_img);
-
 hold on  
-%绘制重心  
-% plot(centroids(:,1), centroids(:,2), 'b*'),  
-%绘制感兴趣区域ROI  
+
 for i=1:size(stats)  
-     rectangle('Position',[stats(i).BoundingBox],'LineWidth',1,'LineStyle','-','EdgeColor','r'),  
+     rectangle('Position',[stats(i).BoundingBox],'LineWidth',1,'LineStyle','-','EdgeColor','g'),  
 end  
 hold off  
+
 figure('name','截取各个区域');
 for i=1:size(stats)  
     I = imcrop(input_img, stats(i).BoundingBox);
     subplot(2,4,i);
     imshow(I);
+    titlename = ['cut',num2str(i)];
+    title(titlename);
+    filename = ['..\output\cut',num2str(i),'.jpg'];
+    imwrite(I,filename);
 end
--- src/thresholding.m
+++
@@ -1,8 +0,0 @@
-function output_img = thresholding( input_img)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    input_img = rgb2gray(input_img);
-%     threshlevel = graythresh(input_img);
-    output_img = im2bw(input_img, 0.75);
-end
-
